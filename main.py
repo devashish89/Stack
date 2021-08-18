bafa8d3db71043551b0ebd102f2845966ee62377
91836 <devashish.nigam89@gmail.com>
2021-08-18 17:34:10 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Stack (LIFO)
+
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        if len(self.container) == 0:
+            return True
+        else:
+            return False
+
+    def size(self):
+        return len(self.container)
+
+    def print_stack(self):
+        for item in self.container:
+            print(item)
+
+s = Stack()
+s.push("www.cnn.com")
+s.push("www.cnn.com/world")
+s.push("www.cnn.com/india")
+s.push("www.cnn.com/china")
+print("last element in stack using peek():", s.peek())
+s.pop()
+s.print_stack()
+print("size:", s.size())
+
+
+
